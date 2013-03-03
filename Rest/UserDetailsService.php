e8b7d45df82682b54d3719c54a088e5dea8ae449
gkhaneja <gourav.khaneja@gmail.com>
2013-03-03 00:52:00 -0800

    getinfo

--- Rest/UserDetailsService.php
+++ Rest/UserDetailsService.php
@@ -1,6 +1,7 @@
 <?php
 require_once('RestService.php');
 require_once('objects/user_details.php');
+require_once('objects/request.php');
 
 class UserDetailsService extends RestService
 {
@@ -15,7 +16,17 @@ class UserDetailsService extends RestService
 		$user_details = new UserDetails();
 		$user_details->get($arguments); 
  }
-	
-	
+
+ public function getInfo($arguments){
+  $request = new Request();
+  if(!isset($arguments['user_id'])){
+   throw new APIException(array("code" =>"3" , 'error' => 'Required Fields are not set.'));
+  }
+  if(isset($arguments['carpool']) && $arguments['carpool']==1){
+   $request->showCarpoolMatches(array($arguments['user_id']));
+  }else{
+   $request->showMatches(array(array('user_id' => $arguments['user_id'], 'percent' => 0)));
+  }
+ }
 	
 }
