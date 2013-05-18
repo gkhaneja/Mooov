7e2cfe6e617f3b184dba68e4d45f910bcecd1f28
gkhaneja <gourav.khaneja@gmail.com>
2013-05-18 15:25:44 -0700

    installer

--- installer.sh
+++ installer.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
-prod='/var/www/html/'
-code='/var/www/Mooov/trunk'
-files=`ls $code`
-echo $files
-sample
 
-function sample() {
+copier()
+{
+  local PASSED=$1
+  if [ -d "${PASSED}" ] ; then
+    files=`ls $PASSED`
+    for file in $files 
+    do
+     copier $PASSED/$file
+    done
+  else
+    if [ -f "${PASSED}" ]; then
+        echo "${PASSED} is a file";
+    else
+        echo "${PASSED} is not valid";
+        exit 1
+    fi
+  fi
+}
+
+code=$1
+prod=$2
+files=`ls $code`
 for file in $files 
 do
- echo $file
+ copier $code/$file
 done
- 
-}
+
