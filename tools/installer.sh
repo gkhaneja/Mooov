26cb8864b9fb2791be8983fa2a726c5c1df118bf
gkhaneja <gourav.khaneja@gmail.com>
2013-05-19 05:38:43 -0700

    installer

--- tools/installer.sh
+++ tools/installer.sh
@@ -1,3 +1,5 @@
+mkdir -p /var/www/html
+
 cp api.php /var/www/html/
 cp getmatches.php /var/www/html/
 cp index.php /var/www/html/
@@ -9,10 +11,15 @@ cp -R tools/ /var/www/html/
 cp -R utils/ /var/www/html/
 cp -R Test/ /var/www/html/
 
-#touch /tmp/mooov.log
+touch /tmp/mooov.log
+chmod 644 /tmp/mooov.log
+chown www-data:www-data /tmp/mooov.log
+
 if [ "$1" = "dev" ]; then
  cp conf/dev.conf /etc/apache2/sites-available/default
+ cp conf/devdb.inc /var/www/html/conf/db.inc
 else
  cp conf/apache.conf /etc/apache2/sites-available/default
 fi
 
+/etc/init.d/apache2 restart
